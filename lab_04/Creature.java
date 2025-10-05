b5097eee6d33131e10d23d910460270750d70f37
Sadia Mohd Doha <sadia.mohddoha32@stu.bmcc.cuny.edu>
2025-10-04 23:44:56 -0400

    Update Creature.java

--- lab_04/Creature.java
+++ lab_04/Creature.java
@@ -1,30 +1,56 @@
 public class Creature {
-    String name;
-    double size;
-    String color;  // Add color property
+    // 1. Attributes 
+    private String name;
+    private double size;
+    private String color;
 
-    // Constructor to initialize Creature
+    // Constructor
     public Creature(String name, double size, String color) {
         this.name = name;
         this.size = size;
-        this.color = color;  // Initialize color
+        this.color = color;
     }
 
-    // Action methods
-    public void sleep() {
-        System.out.println(name + " is sleeping peacefully.");
+    // GETTERS 
+    public String getName() {
+        return name;
+    }
+    public double getSize() {
+        return size;
+    }
+    public String getColor() {
+        return color;
     }
 
-    public void play(String activity) {
-        System.out.println(name + " is playing " + activity + ".");
+    // SETTERS
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setSize(double size) {
+        this.size = size;
+    }
+    public void setColor(String color) {
+        this.color = color;
     }
 
-    public void eat(String food) {
-        System.out.println(name + " eats " + food + ".");
+    //  ACTION METHODS 
+    public String sleep() {
+        return name + " is sleeping peacefully.";
+    }
+    public String play(String activity) {
+        return name + " is playing " + activity + ".";
+    }
+    public String eat(String food) {
+        return name + " eats " + food + ".";
     }
 
-    // Method to display Creature information
-    public void display() {
-        System.out.println(name + " " + size + " " + color);
+    
+    @Override
+    public String toString() {
+        return "--- CREATURE DETAILS ---\n" +
+               "Name: " + name + "\n" +
+               "Size: " + size + "\n" +
+               "Color: " + color + "\n" +
+               "------------------------";
     }
 }
