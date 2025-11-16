3db607f794fef58169fdcd40490388434bd2d2ec
Sadia Mohd Doha <sadia.mohddoha32@stu.bmcc.cuny.edu>
2025-11-16 16:59:32 -0500

    Implement Cell class with creature management

--- lab_07/ecosystem/cell.java
+++ lab_07/ecosystem/cell.java
@@ -1,1 +1,33 @@
+package ecosystem;
 
+import java.util.ArrayList;
+
+public class Cell {
+    private int row, col;
+    private ArrayList<Creature> creatures = new ArrayList<>();
+
+    public Cell(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() { return row; }
+    public int getCol() { return col; }
+
+    public ArrayList<Creature> getCreatures() {
+        return creatures;
+    }
+
+    public void addCreature(Creature c) {
+        creatures.add(c);
+    }
+
+    public void clear() {
+        creatures.clear();
+    }
+
+    @Override
+    public String toString() {
+        return "Cell(" + row + ", " + col + ") Creatures: " + creatures;
+    }
+}
