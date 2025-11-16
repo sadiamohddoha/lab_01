19c5c4dfad3ca72ac1fb0f40b1edb0f92ebec066
Sadia Mohd Doha <sadia.mohddoha32@stu.bmcc.cuny.edu>
2025-11-16 16:58:42 -0500

    Update creature.java

--- lab_07/ecosystem/creature.java
+++ lab_07/ecosystem/creature.java
@@ -1,1 +1,28 @@
+package ecosystem;
 
+public class Creature {
+    private String name;
+    private int energy;
+
+    public Creature(String name, int energy) {
+        this.name = name;
+        this.energy = energy;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public void changeEnergy(int delta) {
+        energy += delta;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (energy=" + energy + ")";
+    }
+}
